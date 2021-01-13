58382a6e4de36a8586688901191487d12042f1ea
iamdbak <37995904+iamdbak@users.noreply.github.com>
2021-01-13 09:14:23 -0800

    Update testAPI.js

--- Server/api/routes/testAPI.js
+++ Server/api/routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res, next){
-   res.send("API is saying Khotu Badhu Bhenchoooooo ");
+   res.send("API is Listening ");
    
 });
 
