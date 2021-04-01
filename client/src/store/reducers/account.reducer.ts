b22a4e52d0f5d2f6659328d35d1511f6518df453
iamdbak <37995904+iamdbak@users.noreply.github.com>
2021-04-01 15:01:56 -0700

    Update account.reducer.ts

--- client/src/store/reducers/account.reducer.ts
+++ client/src/store/reducers/account.reducer.ts
@@ -3,7 +3,7 @@ import { IAccount } from "../models/account.interface";
 import { LOG_IN, LOG_OUT } from "../actions/account.actions";
 
 const initialState: IAccount = {
-    email: "admin@react-template.pl"
+    email: "pk"
 };
 
 function accountReducer(state: IAccount = initialState, action: IActionBase): IAccount {
