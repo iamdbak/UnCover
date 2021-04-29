3c8cf3d48ab8e9c3078b623bbfcd7cebb24e463e
KasinadhuniProgrammer <rachitipidudraksha@gmail.com>
2021-04-29 08:42:49 -0400

    added settings tab

--- client/src/components/Settings/Settings.tsx
+++ client/src/components/Settings/Settings.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-
-//components
-import SettingsHeader from "./SettingsHeader";  
  
 const Settings: React.FC = () => {
     return (
         <div className="settings">
-            <SettingsHeader />
         </div>
     );
 }
