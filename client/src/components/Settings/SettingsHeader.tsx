a5bbb2cf24dd966ec73d0a27a7dd9e03bbe6cecb
KasinadhuniProgrammer <rachitipidudraksha@gmail.com>
2021-04-29 08:36:16 -0400

    added Settings tab to client side

--- client/src/components/Settings/SettingsHeader.tsx
+++ client/src/components/Settings/SettingsHeader.tsx
@@ -1,21 +1,11 @@
 import React, { Fragment } from "react";
 import "./Settings.css"; 
 
+//component will have tags to the Account, Notifications, Contact, Privacy and Safety sections of the Settings Page
+
 const SettingsHeader: React.FC = () => {
     return (
         <div className="settingsheader">
-            <div className="settingsheader__accounttag">
-                <a>Account</a>
-            </div>
-            <div className="settingsheader__notificationstag">
-                <a>Notifications</a>
-            </div>
-            <div className="settingsheader__contacttag">
-                <a>Contact</a>
-            </div>
-            <div className="settingsheader__privacytag">
-                <a>Privacy and Safety</a>
-            </div>
         </div>
     );
 }
